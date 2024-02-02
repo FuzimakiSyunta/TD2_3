d4b4ac843cbcb86285f36677bd005e15f57bc786
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-02-02 17:16:48 +0900

    s

--- DirectXGame/Card.cpp
+++ DirectXGame/Card.cpp
@@ -15,42 +15,8 @@ void Card::Initialize()
 }
 
 void Card::DeckUpdate() {
-	///攻撃カード
-	if (decknumber_ > 0&&decknumber_ <= 10) {
-		isATCcard_ = true;
-		isDEFcard_ = false;
-		isHealcard_ = false;
-		isBufcard_ = false;
-	}
-	///防御カード
-	if (decknumber_ >= 11 && decknumber_ <= 15) {
-		isATCcard_ = false;
-		isDEFcard_ = true;
-		isHealcard_ = false;
-		isBufcard_ = false;
-	}
-	/// 回復カード
-	if (decknumber_ >= 16 && decknumber_ <= 18) {
-		isATCcard_ = false;
-		isDEFcard_ = false;
-		isHealcard_ = true;
-		isBufcard_ = false;
-	}
-	/// バフカード
-	if (decknumber_ >= 19 && decknumber_ <= 20) {
-		isATCcard_ = false;
-		isDEFcard_ = false;
-		isHealcard_ = false;
-		isBufcard_ = true;
-	}
-	///// IMGUI
-	//ImGui::Begin("Decknumber");
-	//ImGui::Text("%d\n", decknumber_);
-	///*ImGui::Text("%d\n", isATCcard_);
-	//ImGui::Text("%d\n", isDEFcard_);
-	//ImGui::Text("%d\n", isHealcard_);
-	//ImGui::Text("%d\n", isBufcard_);*/
-	//ImGui::End();
+	
+	
 }
 
 void Card::HandCardUpdate() {
@@ -58,17 +24,17 @@ void Card::HandCardUpdate() {
 }
 
 void Card::Draw() { 
-	if (isHealcard_==true) {
-		cardSprite_[0]->Draw();
-	}
-	if (isATCcard_==true) {
-		cardSprite_[1]->Draw();
-	}
-	if (isBufcard_==true) {
-		cardSprite_[2]->Draw();
-	}
-	if (isDEFcard_==true) {
-		cardSprite_[3]->Draw();
-	}
+	
+	cardSprite_[0]->Draw();
+	
+	
+	cardSprite_[1]->Draw();
+	
+	
+	cardSprite_[2]->Draw();
+	
+	
+	cardSprite_[3]->Draw();
+	
 }
 
