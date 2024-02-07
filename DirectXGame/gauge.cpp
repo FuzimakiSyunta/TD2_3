f3f6ae97a73c6c35126be499c7199eea72539a9a
ruka <115062996+merutao@users.noreply.github.com>
2024-02-07 20:44:44 +0900

    整理

--- DirectXGame/gauge.cpp
+++ DirectXGame/gauge.cpp
@@ -20,52 +20,57 @@ void Gauge::Update()
 		keyCoolTime_ = 0;
 	}
 
-	if (keyCoolTime_ == 0) {
-		if (gauge >= 0&&gauge<=1100) {
-			// デバッグ用キー
-			if (input_->PushKey(DIK_DOWN)) {
-				gauge -= 1100.0f;
-				keyCoolTime_ = 30;
-			}
-			if (input_->PushKey(DIK_UP)) {
-				gauge += 1100.0f;
-				keyCoolTime_ = 30;
-			}
-			// Aキーを押したときに警戒値ゲージを50下げる
-			if (input_->PushKey(DIK_A)) {
-				gauge -= 50.0f;
-				keyCoolTime_ = 30;
-			}
-			//左キーを押したときに警戒値ゲージを100下げる
-			if (input_->PushKey(DIK_LEFT)) {
-				gauge -= 100.0f;
-				keyCoolTime_ = 30;
-{ 
+	
+
 	if (gaugeCount == 0)
 	{
-		if (gauge_ <= 1000) {
-			if (input_->PushKey(DIK_SPACE)) {
-				gauge_ += 5.0f;
+		if (keyCoolTime_ == 0) {
+			if (gauge >= 0 && gauge <= 1100) {
+				// デバッグ用キー
+				if (input_->PushKey(DIK_DOWN)) {
+					gauge -= 1100.0f;
+					keyCoolTime_ = 30;
+				}
+				if (input_->PushKey(DIK_UP)) {
+					gauge += 1100.0f;
+					keyCoolTime_ = 30;
+				}
+				// Aキーを押したときに警戒値ゲージを50下げる
+				if (input_->PushKey(DIK_A)) {
+					gauge -= 50.0f;
+					keyCoolTime_ = 30;
+				}
+				// 左キーを押したときに警戒値ゲージを100下げる
+				if (input_->PushKey(DIK_LEFT)) {
+					gauge -= 100.0f;
+					keyCoolTime_ = 30;
+				}
+				if (gauge_ <= 1000) {
+					if (input_->PushKey(DIK_SPACE)) {
+						gauge_ += 5.0f;
+					}
+					// Dキーを押したときに警戒値ゲージを50あげる
+					if (input_->PushKey(DIK_D)) {
+						gauge += 50.0f;
+						HPgauge -= 50.0f;
+						keyCoolTime_ = 30;
+					}
+					// 右キーを押したときに警戒値ゲージを100上げる
+					if (input_->PushKey(DIK_RIGHT)) {
+						gauge += 100.0f;
+						HPgauge -= 100.0f;
+						keyCoolTime_ = 30;
+					}
+				}
 			}
-			//Dキーを押したときに警戒値ゲージを50あげる
-			if (input_->PushKey(DIK_D)) {
-				gauge += 50.0f;
-				HPgauge -= 50.0f;
-				keyCoolTime_ = 30;
-			}
-			//右キーを押したときに警戒値ゲージを100上げる
-			if (input_->PushKey(DIK_RIGHT)) {
-				gauge += 100.0f;
-				HPgauge -= 100.0f;
-				keyCoolTime_ = 30;
-			}
-		}
-		}
-		if (gauge_ >= 1000)
-		{
-			gaugeCount = 1;
 		}
 	}
+	if (gauge_ >= 1000)
+	{
+		gaugeCount = 1;
+	
+	}
+	
 	if (gaugeCount == 1)
 	{
 		
